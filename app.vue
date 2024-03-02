08eabaa2fc624f2242f22f7cfae9fbc282073633
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-03-02 12:01:00 +0900

    nuxt re init

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div>
-		<NuxtLayout><NuxtPage /></NuxtLayout>
-	</div>
-</template>
--- pages/index.vue
+++
@@ -1,4 +0,0 @@
-<template>
-	<div class="text-3xl">テキスト</div>
-</template>
-<script setup lang="ts"></script>
