b52dee4cc09961bd1c448a55e76ed34f3760b135
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-03-05 15:31:34 +0900

    feature: chat画面のUIを作成

--- pages/dev.vue
+++ pages/dev.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 	import Header from '~/components/header.vue';
 	import ControllPanel from '~/components/controllpanel.vue';
+	import ChatRoom from '~/components/chatroom.vue';
 	const { metadata, sheetdata } = usePLprotoStore();
 </script>
 <template>
-	<ControllPanel />
+	<ChatRoom />
 </template>
