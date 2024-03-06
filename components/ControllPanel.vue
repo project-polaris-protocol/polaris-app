612d0ad805b3dcdfd232a863dad211515b1ef719
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-03-06 15:47:03 +0900

    feature: W/NのUI部分のパーツ合成

--- components/ControllPanel.vue
+++ components/ControllPanel.vue
@@ -4,25 +4,31 @@
 </script>
 <template>
 	<div>
-		<div class="h-96 flex items-center justify-center">
+		<div class="flex items-center justify-center">
 			<div class="w-64">
+				<div class="divider mt-12 mb-6">ステータス</div>
 				<div class="text-center">
 					<div class="text-sm font-semibold">P-Net (system)</div>
-					<div class="m-2 p-2 bg-emerald-100 border-4 rounded-md font-bold">CONNECT</div>
+					<div class="m-2 p-2 bg-emerald-100 rounded-md font-bold">CONNECT</div>
 				</div>
 				<div class="text-center">
 					<div class="text-sm font-semibold">P-Net (talk)</div>
-					<div class="m-2 p-2 bg-emerald-100 border-4 rounded-md font-bold">CONNECT</div>
+					<div class="m-2 p-2 bg-emerald-100 rounded-md font-bold">CONNECT</div>
 				</div>
 				<div class="text-center">
 					<div class="text-sm font-semibold">Operation</div>
-					<div class="m-2 p-2 bg-emerald-100 border-4 rounded-md font-bold">REMOTE</div>
+					<div class="m-2 p-2 bg-emerald-100 rounded-md font-bold">REMOTE</div>
+				</div>
+				<div class="divider mt-12 mb-6">システム切り替え</div>
+				<div class="text-center mt-6">
+					<button class="btn btn-error btn-outline w-full">オペレーション同期解除</button>
 				</div>
 				<div class="text-center mt-6">
-					<button class="btn btn-error btn-outline">オペレーション同期解除</button>
+					<button class="btn btn-error btn-outline w-full">PAL発砲</button>
 				</div>
 			</div>
 		</div>
+		<div class="divider mt-12 mb-6">操作パネル</div>
 		<div v-if="follow" class="h-96 bg-slate-500 text-white flex items-center justify-center">
 			<div class="border-2 border-white p-6 text-4xl">
 				<div class="text-center mb-3">AUTO</div>
--- pages/display.vue
+++ pages/display.vue
@@ -1,4 +1,22 @@
+<script setup lang="ts">
+	import Header from '~/components/header.vue';
+	import ChatRoom from '~/components/ChatRoom.vue';
+	import LiveScript from '~/components/LiveScript.vue';
+	import ControllPanel from '~/components/ControllPanel.vue';
+</script>
 <template>
-	<div></div>
+	<div>
+		<Header />
+		<div class="flex pt-4">
+			<div class="w-1/3 px-3">
+				<ChatRoom />
+			</div>
+			<div class="w-1/2 px-3">
+				<LiveScript />
+			</div>
+			<div class="w-1/6">
+				<ControllPanel />
+			</div>
+		</div>
+	</div>
 </template>
-<script setup lang="ts"></script>
